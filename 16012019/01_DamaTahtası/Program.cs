e4987f2429d2d6f62b12cbd02b1a88e3fbdf958c
Uğur Deniz <ugur2353@gmail.com>
2019-01-16 14:48:55 +0300

    new

---
+++ 16012019/01_DamaTahtası/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_DamaTahtası
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //* ve # karakterlerini kullanarak 8*8 dama tahtasını ekrana yazan program
+            int kacinciSatir = 0;
+            int satirSayisi = 8;
+            int sutunSayisi = 8;
+
+            for (int i = 0; i < satirSayisi; i++)
+            {
+                for (int j = 0; j < sutunSayisi/2; j++)
+                {
+                    if (kacinciSatir % 2 == 0)
+                    {
+                        Console.Write("*" + " " + "#" + " ");
+                    }
+                    else
+                    {
+                        Console.Write("#" + " " + "*" + " ");
+                    }
+
+                }
+                kacinciSatir++;
+                Console.WriteLine();
+            }
+
+            Console.Read();
+        }
+    }
+}
---
+++ 16012019/02_LotoUygulamasi/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_LotoUygulamasi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] lotoDizisi = new int[6];
+            Random rnd = new Random();
+
+
+            for (int i = 0; i < lotoDizisi.Length; i++)
+            {
+               
+                while (true)
+                {
+                    int uretilenSayi = rnd.Next(50);
+                    bool varMi = DizideVarMi(uretilenSayi, lotoDizisi);
+
+                    if (varMi)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        lotoDizisi[i] = uretilenSayi;
+                        break;
+                    }
+                }
+            }
+
+            for (int i = 0; i < lotoDizisi.Length; i++)
+            {
+                Console.WriteLine(lotoDizisi[i]);
+            }
+
+            Console.Read();
+        }
+
+        private static bool DizideVarMi(int rasgeleSayi, int[] lotoDizisi)
+        {
+            bool varMi = false;
+            for (int i = 0; i < lotoDizisi.Length; i++)
+            {
+                if (lotoDizisi[i] == rasgeleSayi)
+                {
+                    varMi = true;
+                }
+            }
+            return varMi;
+        }
+
+
+    }
+}
